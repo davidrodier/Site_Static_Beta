7105f24ecb178ddca4bb0865b4cd092a15c0ac5c
davidrodier <david_rodier@hotmail.com>
2015-10-26 23:25:17 -0400

    Init

---
+++ Site_Beta/footer.js
@@ -0,0 +1,22 @@
+document.write(
+'<footer style="background-color:#563C4E;" class="page-footer">'+
+          '<div class="container">'+
+            '<div class="row">'+
+              '<div class="col l6 s12">'+
+                '<h5 class="white-text">Challenger Realm</h5>'+
+                '<p class="grey-text text-lighten-4">Sur ce site vous trouverez des informations sur le projet de fin de dec de 3 élèves du collège Lionel-Groulx, leurs information ce retrouve dans l' + "'" + 'onglet contact.</p>'+
+              '</div>'+
+              '<div class="col l4 offset-l2 s12">'+
+                '<h5 class="white-text">Links</h5>'+
+                '<ul>'+
+                  '<li><a class="grey-text text-lighten-3" href="game.html">Jeu</a></li>'+
+                  '<li><a class="grey-text text-lighten-3" href="team.html">Équipe</a></li>'+
+                  '<li><a class="grey-text text-lighten-3" href="#!">Site Officiel</a></li>'+
+                '</ul>'+
+              '</div>'+
+            '</div>'+
+          '</div>'+
+          '<div class="footr-copyright">'+
+          '</div>'+
+        '</footer>'
+);
---
+++ Site_Beta/header.js
@@ -0,0 +1,12 @@
+document.write(
+'<nav>'+
+    '<div style="background-color:#563C4E;" class="nav-wrapper">'+
+      '<a href="index.html" class="brand-logo"><img id="logo" src="res/img/logo.png"/></a>'+
+      '<ul id="nav-mobile" class="right hide-on-med-and-down">'+
+        '<li><a href="game.html">Jeu</a></li>'+
+        '<li><a href="team.html">Équipe</a></li>'+
+        '<li><a href="#!">Site Officiel</a></li>'+
+      '</ul>'+
+    '</div>'+
+  '</nav>'
+);
